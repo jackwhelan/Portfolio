c514b635bb339b6a4532b843106b1c3a14ca39c2
jackwhelan <jackw97@live.ie>
2019-10-10 20:14:51 +0100

    Added about and cv classes

---
+++ classes/about.php
@@ -0,0 +1,9 @@
+<?php
+    namespace classes;
+
+    class about extends controller
+    {
+        var $title = "About";
+        var $local_view = "views/about.phtml";
+        var $view = "views/layout.phtml";
+    }
---
+++ classes/cv.php
@@ -0,0 +1,9 @@
+<?php
+    namespace classes;
+
+    class cv extends controller
+    {
+        var $title = "Curriculum Vitae";
+        var $local_view = "views/cv.phtml";
+        var $view = "views/layout.phtml";
+    }
