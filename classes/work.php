b953e563de128a0adad7cc76cfc79a426c2122c1
jackwhelan <jackw97@live.ie>
2019-10-11 14:50:20 +0100

    Added more pages
    Also added home text.

---
+++ classes/work.php
@@ -0,0 +1,10 @@
+<?php
+    namespace classes;
+
+    class work extends controller
+    {
+        var $title = "Work";
+        var $local_view = "views/work.phtml";
+        var $view = "views/layout.phtml";
+    }
+?>
