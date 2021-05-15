1ac2a2d3b5e26c4e559d81d60fd532c84ff92f4a
Jack Whelan <jackw97@live.ie>
2021-05-15 23:49:27 +0100

    Added boilerplate frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+import Home from './Home';
+
+function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+    );
+}
+
+export default App;
