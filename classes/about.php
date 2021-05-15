d2ff6dc03168da2d0feeabf402eb36272204267e
Jack Whelan <jackw97@live.ie>
2021-05-15 21:12:16 +0100

    Purging previous php mess

--- classes/about.php
+++
@@ -1,9 +0,0 @@
-<?php
-    namespace classes;
-
-    class about extends controller
-    {
-        var $title = "About";
-        var $local_view = "views/about.phtml";
-        var $view = "views/layout.phtml";
-    }
--- classes/cv.php
+++
@@ -1,9 +0,0 @@
-<?php
-    namespace classes;
-
-    class cv extends controller
-    {
-        var $title = "Curriculum Vitae";
-        var $local_view = "views/cv.phtml";
-        var $view = "views/layout.phtml";
-    }
