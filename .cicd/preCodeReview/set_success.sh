f5b22817bb13431108eb0547167610b9e0596eed
Jack Whelan <jackw97@live.ie>
2021-05-16 15:12:23 +0100

    Added build status integration

---
+++ .cicd/preCodeReview/set_success.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export REPO_NAME='portfolio'
+export JOB_NAME='portfolio_PreCodeReview'
+
+curl "//api.github.com/repos/$REPO_NAME/statuses/$GIT_COMMIT?access_token=$GH_TOKEN" \
+-H "Content-Type: application/json" \
+-X POST \
+-d "{
+    \"state\": \"success\",
+    \"context\": \"jenkins/$REPO_NAME\",
+    \"description\": \"Jenkins\",
+    \"target_url\": \"//89.100.10.68:8080/job/$JOB_NAME/$BUILD_NUMBER/console"
+}"
